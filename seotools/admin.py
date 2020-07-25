391731cfb5737d7b04db71c2cdd0c7a21170a8f7
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-07-25 19:06:27 +0530

    removed admin.py

--- seotools/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from seo.admin import ModelInstanceSeoInline
-
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    inlines = [ModelInstanceSeoInline]
