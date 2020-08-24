f9916b83e17db3dfb86c3605cfd667a6d2aa491c
Shreeda Bhat <Shreeda.bhat@yahoo.com>
2020-08-24 21:39:21 +0530

    changes

--- blog/forms.py
+++
@@ -1,8 +0,0 @@
-from mediumeditor.widgets import MediumEditorTextarea
-
-class MyForm(forms.ModelForm):
-    class Meta:
-        model = MyModel
-        widgets = {
-            'my_text_field': MediumEditorTextarea(),
-        }
